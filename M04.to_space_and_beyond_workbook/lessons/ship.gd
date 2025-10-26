0246b63e35ddd08c446fccbab1e5dcab64c19f04
[peeryo] <peeryo@sfusd.edu>
2025-10-26 15:15:09 -0700

    M4L5 completed

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
+
 var boost_speed := 1500.0
 var normal_speed := 600.0
+
 var max_speed := normal_speed
 var velocity := Vector2(0, 0)
-
+var steering_factor := 10.0
 
 
 func _process(delta: float) -> void:
@@ -18,7 +20,10 @@ func _process(delta: float) -> void:
 		max_speed = boost_speed
 		get_node("Timer").start()
 	
-	velocity = direction * max_speed
+	#added during lesson 5
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
